e25f19a3b9aeed61677fe0c3c342f924a3512d15
avandefeniks <angel.vandefeniks@gmail.com>
2022-05-18 15:27:42 -0700

    player object almost done

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -5,6 +5,7 @@ jest.mock('../lib/Potion.js');
 
 
 const Player = require('../lib/Player');
+const { exportAllDeclaration } = require('@babel/types');
 
 
 // test("creates a player as an object", () => {
@@ -39,4 +40,34 @@ test("gets player's stats as an object", () => {
       player.inventory = [];
 
       expect(player.getInventory()).toEqual(false);
+  });
+
+  test("gets player's health value", () => {
+      const player = new Player('Dave');
+
+      expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+  test('checks if player is alive or not', () => {
+      const player = new Player('Dave');
+
+      expect(player.isAlive()).toBeTruthy();
+
+      player.health = 0;
+
+      expect(player.isAlive()).toBeFalsy();
+  });
+
+  test("subtracts from player's health", () => {
+      const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+
   });
